3869e138843f63a40e2927d363d0f6407284012b
Tymzo <stefan.antnj@eduge.ch>
2023-03-03 10:43:20 +0100

    partie 8

---
+++ app/src/main/java/com/example/busschedule/BusScheduleApplication.kt
@@ -0,0 +1,10 @@
+package com.example.busschedule
+
+import AppDatabase
+import android.app.Application
+
+
+
+class BusScheduleApplication : Application(){
+    val database: AppDatabase by lazy { AppDatabase.getDatabase(this) }
+}
